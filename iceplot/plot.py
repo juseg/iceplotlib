26a343b0884705394dccaf92e20106f485426b76
Julien Seguinot <julien.seguinot@natgeo.su.se>
2013-04-03 10:05:07 +0200

    Add a basal temperature map (draft)
    Plot pressure-adjusted basal temperature (need improvements).

--- iceplot/plot.py
+++ iceplot/plot.py
@@ -44,3 +44,32 @@ def icemap(nc, t=0):
     # return velocity image for colormaps
     return im
 
+def basetempmap(nc, t=0):
+    """Draw basal pressure-adjusted temperature map"""
+
+    # extract variables
+    temp  = nc.variables['temppabase'][t].T
+    thk   = nc.variables['thk'][t].T
+    temp  = np.ma.masked_where(thk < 1, temp)
+
+    # draw basal temperature contours
+    levs = [-10, -8, -6, -4, -2, -1e-3, 0]
+    cs = plt.contourf(temp,
+      levels = levs,
+      cmap   = plt.cm.Blues_r,
+      extend = 'min')
+    plt.contour(temp,
+      levels = levs,
+      colors = 'white',
+      linewidths = 0.2,
+      linestyles = 'solid')
+
+    # draw ice outline
+    plt.contour(thk,
+      levels     = [1, 5000],
+      colors     = 'black',
+      linewidths = 1)
+
+    # return contour set for colormaps
+    return cs
+
