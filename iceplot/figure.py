f3454f1a3e7d11bd3f81143350ad111a3be0c353
Julien Seguinot <julien.seguinot@natgeo.su.se>
2013-04-11 14:56:55 +0200

    Replace tabs by spaces
    No code changes here.

--- iceplot/figure.py
+++ iceplot/figure.py
@@ -13,64 +13,64 @@ pad  = 5.
 ### GridFigure class ###
 
 class GridFigure(Figure):
-	"""Gridded figure with arbitrary cols and rows and optional colorbars"""
+  """Gridded figure with arbitrary cols and rows and optional colorbars"""
 
-	def __init__(self, mapsize, nrows_ncols, cbar_mode=None, cbar_location='right', sideplot=False, **kwargs):
-		"""Compute figure size and initialize axes grid"""
+  def __init__(self, mapsize, nrows_ncols, cbar_mode=None, cbar_location='right', sideplot=False, **kwargs):
+    """Compute figure size and initialize axes grid"""
 
-		# compute axes grid size
-		(mapw, maph) = mapsize
-		(rows, cols) = nrows_ncols
-		gridw = cols*(mapw+pad)-pad
-		gridh = rows*(maph+pad)-pad
-		if cbar_mode is 'each' and cbar_location is 'right':
-			gridw += 3*cols*pad
-		if cbar_mode is 'each' and cbar_location is 'top':
-			gridh += 3*cols*pad
-		if cbar_mode is 'single' and cbar_location is 'right':
-			gridw += 3*pad
-		if cbar_mode is 'single' and cbar_location is 'top':
-			gridh += 3*pad
-		#gridsize=(gridw*mm,gridh*mm)
+    # compute axes grid size
+    (mapw, maph) = mapsize
+    (rows, cols) = nrows_ncols
+    gridw = cols*(mapw+pad)-pad
+    gridh = rows*(maph+pad)-pad
+    if cbar_mode is 'each' and cbar_location is 'right':
+      gridw += 3*cols*pad
+    if cbar_mode is 'each' and cbar_location is 'top':
+      gridh += 3*cols*pad
+    if cbar_mode is 'single' and cbar_location is 'right':
+      gridw += 3*pad
+    if cbar_mode is 'single' and cbar_location is 'top':
+      gridh += 3*pad
+    #gridsize=(gridw*mm,gridh*mm)
 
-		# compute figure size
-		figw = gridw + 3*pad
-		figh = gridh + 2*pad
-		if sideplot:
-			figw = figw + figh - 2*pad
+    # compute figure size
+    figw = gridw + 3*pad
+    figh = gridh + 2*pad
+    if sideplot:
+      figw = figw + figh - 2*pad
 
-		# initialize figure
-		Figure.__init__(self, **kwargs)
-		self.set_size_inches(figw*mm,figh*mm)
-		rect = (pad/figw, pad/figh, gridw/figw, gridh/figh)
+    # initialize figure
+    Figure.__init__(self, **kwargs)
+    self.set_size_inches(figw*mm,figh*mm)
+    rect = (pad/figw, pad/figh, gridw/figw, gridh/figh)
 
-		# create axes grid
-		self.grid = ImageGrid(self, rect,
-			nrows_ncols=nrows_ncols, axes_pad=pad*mm,
-			cbar_mode=cbar_mode, cbar_location=cbar_location,
-			cbar_pad=pad*mm, cbar_size=pad*mm)
+    # create axes grid
+    self.grid = ImageGrid(self, rect,
+      nrows_ncols=nrows_ncols, axes_pad=pad*mm,
+      cbar_mode=cbar_mode, cbar_location=cbar_location,
+      cbar_pad=pad*mm, cbar_size=pad*mm)
 
-		# remove ticks
-		for ax in self.grid:
-			ax.get_xaxis().set_visible(False)
-			ax.get_yaxis().set_visible(False)
+    # remove ticks
+    for ax in self.grid:
+      ax.get_xaxis().set_visible(False)
+      ax.get_yaxis().set_visible(False)
 
-		# add more axes
-		if sideplot:
-			self.add_axes([(gridw+4*pad)/figw, 2*pad/figh, (maph-2*pad)/figw, (maph-2*pad)/figh])
+    # add more axes
+    if sideplot:
+      self.add_axes([(gridw+4*pad)/figw, 2*pad/figh, (maph-2*pad)/figw, (maph-2*pad)/figh])
 
 ### Derivative classes ###
 
 class SimpleFigure(GridFigure):
-	"""Simple figure with optional colorbar"""
+  """Simple figure with optional colorbar"""
 
-	def __init__(self, mapsize, **kwargs):
-		GridFigure.__init__(self, mapsize, (1,1), **kwargs)
+  def __init__(self, mapsize, **kwargs):
+    GridFigure.__init__(self, mapsize, (1,1), **kwargs)
 
 class DoubleInlineFigure(GridFigure):
-	"""Double inline figure with optional colorbar"""
+  """Double inline figure with optional colorbar"""
 
-	def __init__(self, mapsize, **kwargs):
-		GridFigure.__init__(self, mapsize, (1,2), **kwargs)
+  def __init__(self, mapsize, **kwargs):
+    GridFigure.__init__(self, mapsize, (1,2), **kwargs)
 
 
