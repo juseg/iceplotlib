99edeef30b064d4ed438b7e20cbc5b59adb06dad
Julien Seguinot <julien.seguinot@natgeo.su.se>
2013-04-23 09:44:34 +0200

    Add two examples
    Draw bed temperature contours and surface velocity quiver.

---
+++ examples/pism_bedtempcontour_demo.py
@@ -0,0 +1,21 @@
+"""
+Draw basal temperature contours.
+"""
+
+from netCDF4 import Dataset
+from matplotlib import pyplot as mplt
+from iceplot import plot as iplt
+from iceplot import autoplot as aplt
+
+# load data
+nc = Dataset('pism_plot_sample.nc')
+
+# plot bed temperature contour
+aplt.bedtempcontour(nc)
+
+# add an ice margin
+iplt.icemargincontour(nc)
+
+# show
+mplt.show()
+
